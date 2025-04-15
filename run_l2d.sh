54c5baf336e80d855e2a28777d5a1ab5d84cee6b
zgjiangtoby <zgjiangtoby@gmail.com>
2025-04-15 16:40:36 +0800

    Update run_l2d.sh

--- run_l2d.sh
+++ run_l2d.sh
@@ -2,14 +2,14 @@
 
 
 CUDA_VISIBLE_DEVICES=0 python L2D.py \
-  --train_path "./all_datasets/cr_data/train.csv" \
-  --test_path "./all_datasets/cr_data/test.csv" \
-  --output_dir "./results/" \
+  --train_path "your/path/to/train.csv" \
+  --test_path "your/path/to/test.csv" \
+  --output_dir "your/path/to/results/" \
   --method L2D \
   --task_name cr \
-  --retriever_model "../LLMs/gte-multilingual-base" \
-  --label_model "../LLMs/xlm-roberta-base" \
-  --llm_model "../LLMs/Qwen2.5-7B-Instruct" \
+  --retriever_model "your/path/to/retirever" \
+  --label_model "your/path/to/SLM" \
+  --llm_model "your/path/to/LLM" \
   --seed 521 \
   --alpha 0.5 \
   --gpu 1 \
